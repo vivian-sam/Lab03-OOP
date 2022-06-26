44a2def8740aef910ffdbed20832a0bfc503c263
vivian-sam <56103899+vivian-sam@users.noreply.github.com>
2022-06-26 15:42:46 -0700

    Add files via upload

---
+++ Lab03 OOP/Lab03 OOP/myStack.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+
+namespace Lab03
+{
+	class MyStack
+	{
+	private:
+		static const int stackSize = 10;
+		int stackArr[stackSize];
+		int stackIndex = 0;
+
+	public:
+
+	};
+
+
+}
